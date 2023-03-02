eb44d5b68897eccae01da19f43b6201e611b2620
Riley <riley.freier@gmail.com>
2023-03-02 13:26:15 -0700

    is done, ik the grammer is incorrect but its funny

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+// removing #main
+const element = document.getElementById("main");
+element.remove();
+
+//new header node added
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "I is the champion"
+newHeader.appendChild();
